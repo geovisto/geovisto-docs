645314fc6eccd84b576d69398b9b6e40e5da9a66
tkosci <tomas.koscielniak@gmail.com>
2022-04-06 21:56:52 +0200

    Proof of concept done. Sucessfully integrated geovisto

---
+++ src/pages/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import styles from './index.module.css';
+import BrowserOnly from '@docusaurus/BrowserOnly';
+
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro">
+            Docusaurus Tutorial - 5min ⏱️
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home(): JSX.Element {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Geovisto`}
+      description="Description will go into a meta tag in <head />">
+      <HomepageHeader />
+      <main>
+        <Render_geovisto />
+      </main>
+    </Layout>
+  );
+}
+
+export function Render_geovisto(): JSX.Element {
+    return (
+        <BrowserOnly fallback={<div>Loading...</div>}>
+            {() => {
+                const Choropleth = require('../geovisto_demos/choropleth').default;
+                return <Choropleth />;
+            }}
+        </BrowserOnly>
+    );
+}
