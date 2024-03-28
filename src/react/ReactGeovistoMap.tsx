3f83018cbd5f95deff4f6785d8995aed4e38c4ad
Iva Utikalova <iva.utikalova@dmdox.com>
2024-03-28 22:50:35 +0100

    listener, redraw

--- src/react/ReactGeovistoMap.tsx
+++ src/react/ReactGeovistoMap.tsx
@@ -2,7 +2,18 @@
 import React, { useEffect, useRef } from "react";
 
 // Geovisto
-import { Geovisto, IMap, IMapProps } from "geovisto";
+import { Geovisto, 
+        IMap, 
+        LayerToolRenderType, 
+        IMapEventListener, 
+        GeoDataManagerChangeEvent, 
+        GeoDataChangeEvent, 
+        DataManagerChangeEvent, 
+        DataChangeEvent,
+        ToolEnabledEvent,
+        LayerToolRenderedEvent,
+        LayerToolDimensionChangeEvent 
+    } from "geovisto";
 import IReactGeovistoMapProps from "./IReactGeovistoMapProps";
 
 // styles
@@ -18,13 +29,70 @@ import "./common.css";
  */
 const ReactGeovistoMap: React.FC<IReactGeovistoMapProps> = (props) => {
     const mapRef = props.ref ?? useRef<IMap>();
- 
+    const helpRef = useRef(false);
+
+    //listener for events in map
+    const listener: IMapEventListener = {
+        handleEvent(event) {
+            console.log("handler");
+            //console.log(event);
+            console.log(event.getType());
+            switch (event.getType()) {
+                case GeoDataManagerChangeEvent.TYPE():
+                    console.log(GeoDataManagerChangeEvent.TYPE());
+                    break;
+                case GeoDataChangeEvent.TYPE():
+                    console.log(GeoDataChangeEvent.TYPE());
+                    break;
+                case DataManagerChangeEvent.TYPE():
+                    console.log(DataManagerChangeEvent.TYPE());
+                    break;
+                case DataChangeEvent.TYPE():
+                    console.log(DataChangeEvent.TYPE());
+                    break;
+                case ToolEnabledEvent.TYPE():
+                    console.log(ToolEnabledEvent.TYPE());
+                    break;
+                case LayerToolRenderedEvent.TYPE():
+                    console.log(LayerToolRenderedEvent.TYPE());
+                    break;
+                case LayerToolDimensionChangeEvent.TYPE():
+                    console.log(LayerToolDimensionChangeEvent.TYPE());
+                    break;
+            
+                //case this.getSelectionTool()?.getChangeEventType():
+                    //console.log(this.getSelectionTool()?.getChangeEventType());
+                    //this.render(LayerToolRenderType.STYLE);
+                    //break;
+                //case this.getThemesTool()?.getChangeEventType():
+                    //console.log(this.getThemesTool()?.getChangeEventType());
+                    //mapRef.current.getState().
+                    //this.updateTheme((<IThemesToolEvent> event).getChangedObject());
+                    //this.render(LayerToolRenderType.STYLE);
+                    //break;
+        
+                default:
+                    break;
+            }
+            
+            //getting config
+            const config = mapRef.current.export(); 
+            console.log(config);
+            (document.getElementById('config') as HTMLInputElement).value = JSON.stringify(config, null, 4);
+        }   
+    };
+
+    
     const mapContainerRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
-        if (mapContainerRef.current) {
-            // create new Geovisto map
-            mapRef.current = Geovisto.createMap(props);
+        // create new Geovisto map
+        mapRef.current = Geovisto.createMap(props);
+
+        if (!helpRef.current) {
+
+            // adding listener
+            //mapRef.current.getState().getEventManager().addEventListener(listener);
 
             // draw map with the current config
             // timeout is set to fix crashing with leaflet
@@ -34,14 +102,19 @@ const ReactGeovistoMap: React.FC<IReactGeovistoMapProps> = (props) => {
                         Geovisto.getMapConfigManagerFactory().default({})
                 );
             }, 0);
-        }
 
-        return () => {
-            if (mapRef.current) {
-                mapRef.current = undefined;
-            }
-        };
-    }, []);
+            helpRef.current = true;
+
+        } else {
+            console.log("redraw");
+            setTimeout(() => {
+                mapRef.current.redraw(
+                    props.config ?? Geovisto.getMapConfigManagerFactory().default({}), props);
+            }, 0);
+        } 
+        console.log("mapRef");
+        console.log(mapRef.current);
+    }, [props]);
 
 
     return (
