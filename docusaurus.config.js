1d40482b076ac07502b2fec44e4a28c12cea9b3a
Iva Utikalova <iva.utikiv@gmail.com>
2024-05-02 00:52:18 +0200

    static fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  staticDirectories: ['public', 'static'],
+  staticDirectories: ['static'],
   favicon: 'img/geovisto-logo4.png',
   organizationName: 'geovisto', // Usually your GitHub org/user name.
   projectName: 'geovisto.github.io', // Usually your repo name.
